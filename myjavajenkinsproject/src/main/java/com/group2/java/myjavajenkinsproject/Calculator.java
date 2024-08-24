c757cec1cfbb39084f3229ce4c4b86de3458214f
javagroupproject2 <javagroupproject2@gmail.com>
2024-08-24 12:15:13 -0500

    upload first project

---
+++ myjavajenkinsproject/src/main/java/com/group2/java/myjavajenkinsproject/Calculator.java
@@ -0,0 +1,15 @@
+
+package com.group2.java.myjavajenkinsproject;
+
+
+public class Calculator {
+    
+    public int add (int x, int y) {
+        return x + y;
+    }
+    
+    public int mul (int x, int y) {
+        return x * y;
+    }
+    
+}
---
+++ myjavajenkinsproject/src/main/java/com/group2/java/myjavajenkinsproject/Myjavajenkinsproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group2.java.myjavajenkinsproject;
+
+/**
+ *
+ * @author wende
+ */
+public class Myjavajenkinsproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
